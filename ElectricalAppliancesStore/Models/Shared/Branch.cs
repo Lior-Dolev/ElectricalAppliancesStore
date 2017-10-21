16443b24e2600b4641a2c5cf84e6adf78c81e699
Boberte <myjunkmailr1@gmail.com>
2017-10-21 17:01:36 +0300

    First Additions
    Added the models and the first index (Login page)

---
+++ ElectricalAppliancesStore/Models/Shared/Branch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElectricalAppliancesStore.Models.Clients
+{
+    public class Branch
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string OpeningHours { get; set; }
+        public string Address { get; set; }
+        public string PNG_Path { get; set; }
+    }
+}
---
+++ ElectricalAppliancesStore/Models/Shared/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElectricalAppliancesStore.Models.Shared
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SupplierID { get; set; }
+        public String Category { get; set; }
+        public String Brand { get; set; }
+        public String PNG_Path { get; set; }
+        public String Description { get; set; }
+        public double buyPrice { get; set; }
+        public double salePrice { get; set; }
+        public int quantity { get; set; }
+        public int salesCounter { get; set; }
+    }
+}
