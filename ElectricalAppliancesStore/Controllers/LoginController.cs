4f7093a614a2e77736587948f485c38a80467ea5
Lior Dolev <liordolev7@gmail.com>
2017-10-22 09:21:08 +0300

    add files before merge with Ravid

---
+++ ElectricalAppliancesStore/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectricalAppliancesStore.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(Models.User user)
+        {
+            if (validate(user))
+            {
+                return RedirectToAction("Index", "Products");
+            }
+            return View("Index",user);
+        }
+
+        public bool validate(Models.User user)
+        {
+            if(!string.IsNullOrEmpty(user.UserName) && !string.IsNullOrEmpty(user.Password))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
