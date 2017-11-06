0316a42b5f4052d6d0da2c24873c8d13b9b35096
Boberte <myjunkmailr1@gmail.com>
2017-11-06 19:39:12 +0200

    Added the new logic for adding item validation

--- ElectricalAppliancesStore/Controllers/ProductViewController.cs
+++ ElectricalAppliancesStore/Controllers/ProductViewController.cs
@@ -10,17 +10,63 @@ namespace ElectricalAppliancesStore.Controllers
 {
     public class ProductViewController : Controller
     {
+        private ProductView pv;
+        private List<Product> products;
         // GET: ProductView
         public ActionResult Products()
         {
-            List<Product> products = ProductsStub.GetProducts();
-            ProductView pv = new ProductView()
+            this.products = ProductsStub.GetProducts();
+            this.pv = new ProductView()
             {
                 products = products,
                 currOrder = OrderStub.GetOrder()
-            //currOrder = new Order()
-        };
+                //currOrder = new Order()
+            };
             return View(pv);
+        }
+
+        public bool UpdateOrder(int itemID, int Quantity)
+        {
+
+            try
+            {
+                // check if such item exist
+                Product prod = this.products.First<Product>(p => (p.ID == itemID));
+
+                // if so- check if this id is allready in the order's item-list
+                if (prod != null)
+                {
+                    OrderItem oit = this.pv.currOrder.Items.First<OrderItem>(o => o.Product.ID == itemID);
+
+                    if (oit == null)
+                    {
+                        // if not - add the item with the given amount (if amount is not 0)
+                        if (Quantity != 0)
+                        {
+                            this.pv.currOrder.Items.Add(new OrderItem() { ID = itemID, Quantity = Quantity, Product = prod });
+                        }
+                    }
+                    //if item is allready in the order 
+                    else
+                    {
+                        // check if given amount is 0 - and if so - remove the item from the order
+                        if (Quantity == 0)
+                        {
+                            this.pv.currOrder.Items.Remove(oit);
+                        }
+                        // otherwise - update the item's amount in the order
+                        else
+                        {
+                            oit.Quantity = Quantity;
+                        }
+                    }
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
