e1b30825b7d94004c34003f99d20635fa36ed80f
Boberte <myjunkmailr1@gmail.com>
2017-11-07 22:35:13 +0200

    minor fix

--- ElectricalAppliancesStore/Controllers/LoginController.cs
+++ ElectricalAppliancesStore/Controllers/LoginController.cs
@@ -53,10 +53,10 @@ namespace ElectricalAppliancesStore.Controllers
 
             if(users.Exists(stubUser => (stubUser.Username == user.Username) && (stubUser.Password == user.Password)))
             {
-                if ( (u.Username == user.Username) && (u.Password == user.Password)) {
-                    permission = u.PermissionType;
+                //if ( (u.Username == user.Username) && (u.Password == user.Password)) {
+                    permission = user.PermissionType;
                     return true;
-                }
+                //}
             }
             return false;
         }
